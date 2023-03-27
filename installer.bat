b17ba32085b0033502a8e74064070a760172d222
planettbh <74864510+planettbh@users.noreply.github.com>
2023-03-26 19:56:14 -0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,2 +1,8 @@
-pip install colorama
 pip install pyautogui
+pip install time
+pip install random
+pip install string
+pip install os
+pip install ctypes
+pip install getpass
+pip install pygetwindow
